1ce603d870c709d0d49e37fd867b6e1fab8d4e8c
Dennis Gera <geradennis@gmail.com>
2024-01-15 08:57:23 -0300

    fix: schema folder name

--- app/api/v1/endpoints/materials.py
+++ app/api/v1/endpoints/materials.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from app.db.database import get_db
+from app.schemas.material import Material, MaterialCreate
+
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/materials/", response_model=Material)
+def create_material(material: MaterialCreate, db: Session = Depends(get_db)):
+    ...
