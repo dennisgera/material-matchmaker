ee7dfeb30ade24fcea2a5bb892f0cae819f7cbce
Dennis Gera <geradennis@gmail.com>
2024-01-22 08:58:25 -0300

    feat: add more material endpoints

--- app/api/v1/endpoints/materials.py
+++ app/api/v1/endpoints/materials.py
@@ -1,7 +1,7 @@
 from typing import List
 from fastapi import APIRouter, Depends, status
 from app.controllers.material_controller import MaterialController
-from app.schemas.material import Material
+from app.schemas.material import Material, MaterialCreate, MaterialUpdate
 from app.api import deps
 
 router = APIRouter()
@@ -12,9 +12,24 @@ def get_materials(
 ):
     return controller.get_all()
 
-@router.post("/", response_model=Material, status_code=status.HTTP_201_CREATED)
-def create_material(
-    material: Material,
+@router.get("/{id_material}", response_model=Material)
+def get_material_by_id(
+    id_material: int,
     controller: MaterialController = Depends(deps.get_material_controller),
 ):
-    return controller.create(material)
+    return controller.get_by_id(id_material=id_material)
+
+@router.post("/", response_model=Material, status_code=status.HTTP_201_CREATED)
+def create_material(
+    material: MaterialCreate,
+    controller: MaterialController = Depends(deps.get_material_controller),
+):
+    return controller.create(create=material)
+
+@router.patch("/{id_material}", response_model=Material)
+def update_material(
+    id_material: int,
+    material: MaterialUpdate,
+    controller: MaterialController = Depends(deps.get_material_controller),
+):
+    return controller.update(update=material, id_material=id_material)
